24386246a2f9c15b35634297d78a3f83b7eba502
julia.placzkowska <jplaczkowska6@gmail.com>
2022-05-09 02:13:29 +0200

    set default request mapping value /api/v1

--- src/main/java/pl/example/currencycalculator/controller/ExchangeController.java
+++ src/main/java/pl/example/currencycalculator/controller/ExchangeController.java
@@ -4,12 +4,14 @@ package pl.example.currencycalculator.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.example.currencycalculator.service.ExchangeService;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1")
 public class ExchangeController {
 
     private final ExchangeService exchangeService;
