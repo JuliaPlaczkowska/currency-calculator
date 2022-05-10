fc06eb8dce8a1bbd6c7cded2d5aa55b23917592e
julia.placzkowska <jplaczkowska6@gmail.com>
2022-05-10 09:59:42 +0200

    code cleanup

--- src/main/java/pl/example/currencycalculator/service/ExchangeService.java
+++ src/main/java/pl/example/currencycalculator/service/ExchangeService.java
@@ -2,6 +2,8 @@ package pl.example.currencycalculator.service;
 
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pl.example.currencycalculator.exceptions.InvalidCurrencyCodeException;
@@ -11,22 +13,37 @@ import pl.example.currencycalculator.model.dto.CurrencyDto;
 import pl.example.currencycalculator.model.dto.TableDto;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static pl.example.currencycalculator.constants.ApiConstants.NBP_API_URL;
+import static pl.example.currencycalculator.constants.ApiConstants.NBP_TABLE_A_ENDPOINT;
 
 
 @AllArgsConstructor
 @Service
 public class ExchangeService {
 
-    private static final String URL = "https://api.nbp.pl/api/exchangerates/tables/a";
+    private static final String URL = NBP_API_URL + NBP_TABLE_A_ENDPOINT;
     private final RestTemplate restTemplate = new RestTemplate();
+    private final ActivityLogService activityLogService;
+
+    private TableDto[] getTableAFromNbpApi(){
+        ResponseEntity<TableDto[]> response = restTemplate.getForEntity(URL, TableDto[].class);
+        HttpStatus statusCode = response.getStatusCode();
+        activityLogService.saveNbpGetTableA(statusCode.toString(), LocalDateTime.now());
+
+        return response.getBody();
+    }
 
     private List<CurrencyDto> getAll() throws NoContentException {
-        TableDto[] table = restTemplate.getForObject(URL, TableDto[].class);
+        TableDto[] table = getTableAFromNbpApi();
         if (table == null)
             throw new NoContentException();
         else
@@ -78,8 +95,11 @@ public class ExchangeService {
 
             result = valueAsk * currencyAsk.getMid() / currencyBid.getMid();
         }
+
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.ENGLISH);
+
         String decimalFormat = "0.00";
-        NumberFormat formatter = new DecimalFormat(decimalFormat);
+        NumberFormat formatter = new DecimalFormat(decimalFormat, symbols);
         return formatter.format(result);
     }
 
